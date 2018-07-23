6907d1d324a22ca48a2c79d7221688d82f579417
varsha2611 <varshac@g.clemson.edu>
2018-07-23 13:04:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ algorithm.run(2000)
 feasible_solutions = [s for s in algorithm.result if s.feasible]
 
 # plot the results using matplotlib
+'''
 import matplotlib.pyplot as plt
 
 plt.scatter([s.objectives[0] for s in feasible_solutions],
@@ -14,3 +15,4 @@ plt.scatter([s.objectives[0] for s in feasible_solutions],
 plt.xlabel("$PRI(x)$")
 plt.ylabel("$Cost(x)$")
 plt.show()
+'''
